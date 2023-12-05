556b0924c4c6d4a0e7375580af7d7ea3c57833ce
velcrots <jehwison@o.cnu.ac.kr>
2023-12-05 16:45:26 +0900

    feat: implement enhanced random string generation - Updated StringGenerator class to use a more robust random engine - Modified generateRandomString function to ensure the generated string has a length between 2 and 20 characters - Refactored code for improved readability and maintainability

--- SpeedTyper/StringGenerator.h
+++ SpeedTyper/StringGenerator.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef STRINGGENERATOR_H
+#define STRINGGENERATOR_H
+
+#include <string>
+#include <random>
+
+class StringGenerator {
+private:
+    std::random_device rd;
+    std::mt19937 randomEngine;
+
+public:
+    StringGenerator();
+    std::string generateRandomString(int minLength, int maxLength);
+};
+
+#endif // STRINGGENERATOR_H
